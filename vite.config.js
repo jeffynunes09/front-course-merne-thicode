b22fcbb6e2c00d8374b90146d069cb985fda9022
Jefferson Nunes <96424928+jeffynunes09@users.noreply.github.com>
2024-06-26 22:37:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      '/': {
         target: 'https://course-mern-thicode.onrender.com/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
